dd0113b85b7f2612ea57e881a11f6c4f9dd31722
grammers <samuel_karlsson@outlook.com>
2024-02-05 10:45:31 +0100

    fixing path issue

--- behaviorTreeRos/launch/shafter_take_off_launcher.py
+++ behaviorTreeRos/launch/shafter_take_off_launcher.py
@@ -1,7 +1,11 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    rfa = get_package_share_directory('behavior_tree_ros')
+    user = os.getlogin()
     return LaunchDescription([
         Node(
             package="behavior_tree_ros",
@@ -13,7 +17,8 @@ def generate_launch_description():
             parameters=[
                 #{"tree_main" : "Hover"},
                 {"tree_main" : "behaviorTreeLab"},
-                {"tree_path" : "/home/grammers/colcon_ws/behviorTree/src/behaviorTreerRos/trees/"},
+                {"tree_path" : "/home/" + user + "/colcon_ws/src/R0006E-Robotics-for-all-behavior-tree/behaviorTreeRos/trees/"},
+                #{"tree_path" : rfa + '/trees'}, #os.path.join(rfa, 'trees')},
                 {"take_off_topic" : "start"},
                 {"safety_land_topic" : "safety_land"},
                 {"flying_status_topic" : "flying"},
